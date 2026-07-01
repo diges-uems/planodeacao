5a8f30377601c60ef3a043d1656a9b54dcb6a558
diges-uems <enade@uems.br>
2026-07-01 12:20:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/planodeacao/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
